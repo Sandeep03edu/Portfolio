d2c987899615a5ce56195af1c87e8169091d27be
Sandeep Mishra <sandeep03edu@gmail.com>
2024-01-16 18:24:20 +0530

    Updated Education Item

--- src/Components/Education.js
+++ src/Components/Education.js
@@ -5,54 +5,60 @@ export default function Education(props) {
   let cardBackgroundColor = "#fff";
   let textColor = "#000";
   return (
-    <div
-      className="shadow p-3  rounded"
-      style={{
-        marginTop: "8%",
-        marginRight: "1%",
-        marginLeft: "1%",
-        backgroundColor: props.cardBackground,
-        color: props.textColor,
-      }}
-    >
-      <h2
-        className="center-container header-style"
-        style={{ fontFamily: "Manrope" }}
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div
+        className="shadow p-3  rounded"
+        style={{
+          marginTop: "8%",
+          marginRight: "1%",
+          marginLeft: "1%",
+          backgroundColor: props.cardBackground,
+          color: props.textColor,
+          width: "70%",
+        }}
       >
-        Education
-      </h2>
-      <div className="row my-4">
-        <div className="col-md-12">
-          <EducationItem
-            cardBackground={cardBackgroundColor}
-            textColor={textColor}
-            url="https://www.iitp.ac.in/"
-            imageUrl={process.env.PUBLIC_URL + "/iitp_campus.webp"}
-            type="College"
-            schoolName="Indian Institute Of Technology, Patna"
-            details={[
-              "Duration: 2020 - Present",
-              "Btech. Electrical and Electronics Engineering",
-              "CPI: 8.77 (6th Semester)",
-            ]}
-            reverse={false}
-          />
-        </div>
-        <div className="col-md-12">
-          <EducationItem
-            cardBackground={cardBackgroundColor}
-            textColor={textColor}
-            url="https://www.facebook.com/rpvvpaschimvihar/"
-            imageUrl={process.env.PUBLIC_URL + "/rpvv.jpg"}
-            type="Intermediate"
-            schoolName="Rajkiya Pratibha Vikas Vidyalaya A-6 Paschim Vihar"
-            details={[
-              "Duration: 2018-2020",
-              "12th Central Board Of Secondary Education",
-              "Percentage: 95.8%",
-            ]}
-            reverse={true}
-          />
+        <h2
+          className="center-container header-style"
+          style={{ fontFamily: "Manrope" }}
+        >
+          Education
+        </h2>
+        <div className="row my-4">
+          <div className="col-md-12">
+            <EducationItem
+              cardBackground={cardBackgroundColor}
+              textColor={textColor}
+              url="https://www.iitp.ac.in/"
+              imageUrl={process.env.PUBLIC_URL + "/iitp_campus.webp"}
+              type="College"
+              schoolName="Indian Institute Of Technology, Patna"
+              details={[
+                [["Duration:"], ["2020 - 2024 (Expected)"]],
+                [
+                  ["Degree:"],
+                  ["Btech. Electrical and Electronics Engineering"],
+                ],
+                [["CPI:"], ["8.77 (6th Semester)"]],
+              ]}
+              reverse={false}
+            />
+          </div>
+          <div className="col-md-12">
+            <EducationItem
+              cardBackground={cardBackgroundColor}
+              textColor={textColor}
+              url="https://www.facebook.com/rpvvpaschimvihar/"
+              imageUrl={process.env.PUBLIC_URL + "/rpvv.jpg"}
+              type="Intermediate"
+              schoolName="Rajkiya Pratibha Vikas Vidyalaya A-6 Paschim Vihar"
+              details={[
+                [["Duration:"], ["2018-2020"]],
+                [["Board:"], ["12th Central Board Of Secondary Education"]],
+                [["Percentage:"], ["95.8%"]],
+              ]}
+              reverse={true}
+            />
+          </div>
         </div>
       </div>
     </div>
--- src/Components/EducationItem.js
+++ src/Components/EducationItem.js
@@ -3,71 +3,97 @@ import React from "react";
 export default function EducationItem(props) {
   return (
     <div
-      className="shadow rounded p-3 row"
-      style={
-        props.reverse === true
-          ? {
-              backgroundColor: props.cardBackground,
-              color: props.textColor,
-              marginBottom: -20,
-              display: "flex",
-              flexDirection: "row-reverse",
-            }
-          : {
-              backgroundColor: props.cardBackground,
-              color: props.textColor,
-              marginBottom: 40,
-            }
-      }
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
     >
-      <div className="col-md-6">
-        <img
-          className="my-2"
-          src={props.imageUrl}
-          style={{ maxWidth: "100%", width: "100%", height: "auto" }}
-          alt="educationImage"
-        />
-      </div>
-
-      <div className="col-md-6">
+      <div
+        className=" rounded p-3 row"
+        style={
+          props.reverse === true
+            ? {
+                backgroundColor: props.cardBackground,
+                color: props.textColor,
+                marginBottom: -20,
+                display: "flex",
+                flexDirection: "row-reverse",
+                alignItems: "center",
+                width: "100%",
+              }
+            : {
+                backgroundColor: props.cardBackground,
+                color: props.textColor,
+                marginBottom: 40,
+                alignItems: "center",
+                width: "100%",
+              }
+        }
+      >
         <div
           style={{
             textAlign: "center",
           }}
         >
-          <h5 className="my-2 centered header2-style ">
+          <p className="my-2 mx-0 centered" style={{fontSize:"1.35rem"}}>
             <b>{props.schoolName}</b>
-          </h5>
-        </div>
-        <div className="container my-3">
-          <ul>
-            {props.details &&
-              props.details.map((data) => {
-                return (
-                  <ul>
-                    <li className="description-style" key={data}>
-                      {data}
-                    </li>
-                  </ul>
-                );
-              })}
-          </ul>
+          </p>
         </div>
         <div
+          className="col-lg-6 col-xl-5 rounded"
           style={{
-            textAlign: "center",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
           }}
         >
-          <h4>
-            <a
-              className="bold-underline-link"
-              href={props.url}
-              target="_blank"
-              rel="noreferrer"
-            >
-              More Details
-            </a>
-          </h4>
+          <img
+            className="my-2"
+            src={props.imageUrl}
+            style={{ width: "100%", height: "auto", overflowY: "scroll" }}
+            alt="educationImage"
+          />
+        </div>
+
+        <div className="col-lg-6 col-xl-7">
+          <div className="container my-3">
+            {props.details &&
+              props.details.map((data, index) => (
+                <div className="row mb-2 p-0" key={index}>
+                  <div className="form-floating p-0 m-0">
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="floatingLabel"
+                      placeholder="Password"
+                      readOnly
+                      value={data[1]}
+                      style={{ fontSize: "1rem" }}
+                    />
+                    <label for="floatingLabel" style={{ fontSize: "1rem" }}>
+                      {data[0]}
+                    </label>
+                  </div>
+                </div>
+              ))}
+          </div>
+          <div
+            style={{
+              textAlign: "center",
+            }}
+          >
+            {/* <button type="button" class="btn btn-outline-primary">
+              <a
+                href={props.url}
+                target="_blank"
+                rel="noreferrer"
+                style={{ textDecoration: "none" }}
+              >
+                More Details
+              </a>
+            </button> */}
+          </div>
         </div>
       </div>
     </div>
