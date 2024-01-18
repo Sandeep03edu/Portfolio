d4fc48d0d75cd709d532415cb51ffdbbefa4b539
Sandeep Mishra <sandeep03edu@gmail.com>
2024-01-18 16:25:20 +0530

    Updated Experience page

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -6,153 +6,129 @@ export default function Experience(props) {
   let textColor = "#000";
 
   return (
-    <div
-      className="shadow p-3 rounded"
-      style={{
-        marginTop: "8%",
-        marginRight: "1%",
-        marginLeft: "1%",
-        backgroundColor: props.cardBackground,
-        color: props.textColor,
-      }}
-    >
-      <h2
-        className="center-container header-style"
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div
+        className="shadow p-3 rounded"
         style={{
-          fontFamily:"Manrope",
-          marginTop: "1%",
-          marginBottom: "2%",
+          marginTop: "8%",
+          marginRight: "1%",
+          marginLeft: "1%",
+          backgroundColor: props.cardBackground,
+          color: props.textColor,
+          width: "80%",
         }}
       >
-        Work Experience
-      </h2>
+        <h2
+          className="center-container header-style"
+          style={{
+            fontFamily: "Manrope",
+            marginTop: "1%",
+            marginBottom: "2%",
+          }}
+        >
+          Work Experience
+        </h2>
 
-      <div
-        id="carouselExampleIndicators"
-        className="carousel slide"
-        data-ride="carousel"
-      >
-        <ol className="carousel-indicators">
-          <li
-            data-bs-target="#carouselExampleIndicators"
-            data-slide-to="0"
-            className="active"
-          ></li>
-          <li
-            data-bs-target="#carouselExampleIndicators"
-            data-slide-to="1"
-          ></li>
-          <li
-            data-bs-target="#carouselExampleIndicators"
-            data-slide-to="2"
-          ></li>
-          <li
-            data-bs-target="#carouselExampleIndicators"
-            data-slide-to="3"
-          ></li>
-        </ol>
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <ExperienceItem
-              cardBackground={cardBackgroundColor}
-              textColor={textColor}
-              role="EDG Intern"
-              urlType="Certificate"
-              name="Mathworks"
-              imageUrl={process.env.PUBLIC_URL + "/mathworks.png"}
-              url="https://drive.google.com/file/d/1acCiod5-fUc1rjB39_WfZqW0ZsJCN1Ld/view"
-              languageUsed="Matlab, C++"
-              toolsUsed="Matlab App Designer, Simulink, Matlab GUI"
-              duration="May 2023 - July 2023"
-              details={[
-                "Worked on the User Interface for MATLAB Simulink Real-Time Explorer target management.",
-                "Enhanced the GUI of the explorer using MATLAB App Designer.",
-                "Conducted automated testing of Explorer GUI actions and Backend API calls.",
-              ]}
-            />
-          </div>
+        <div>
+          <div>
+            <div>
+              <ExperienceItem
+                cardBackground={cardBackgroundColor}
+                textColor={textColor}
+                urlType="Certificate"
+                organization="MathWorks India Private Limited"
+                imageUrl={process.env.PUBLIC_URL + "/mathworks.png"}
+                url="https://drive.google.com/file/d/1acCiod5-fUc1rjB39_WfZqW0ZsJCN1Ld/view"
+                details={[
+                  ["Role", "EDG Intern"],
+                  ["Duration", "May 2023 - July 2023"],
+                ]}
+                details2={[
+                  [
+                    "Work",
+                    "Worked on the User Interface for MATLAB Simulink Real-Time Explorer target management",
+                    "Enhanced the GUI of the explorer using MATLAB App Designer",
+                    "Conducted automated testing of Explorer GUI actions and Backend API calls.",
+                  ],
+                  ["Tools Used", "Matlab App Designer, Simulink, Matlab GUI"],
+                  ["Language Used", "Matlab, C++"],
+                ]}
+              />
+            </div>
 
-          <div className="carousel-item">
-            <ExperienceItem
-              cardBackground={cardBackgroundColor}
-              textColor={textColor}
-              role="Project Intern"
-              urlType="Certificate"
-              name="Indian Army"
-              imageUrl={process.env.PUBLIC_URL + "/indianArmy.png"}
-              url="https://drive.google.com/file/d/1GG3aImLWPqQCCX1ZPVX9nIycRrzDPt8y/view?usp=sharing"
-              languageUsed="Java, XML, Kotlin, C++, Smali, Sqlite"
-              toolsUsed="Apktool, Frida, Jadx-Gui, JD-Gui, Retrofit"
-              duration="July 2021 - Present"
-              details={[
-                "Created POC (Proof of concept) for some working malwares (like Anubis) and tested their functionality",
-                "Analyzed android security, loop holes, Android Internals working.",
-                "Reverse engineering, detection , analysis of malwares working and Android Smali Patching.",
-              ]}
-            />
-          </div>
+            <div>
+              <ExperienceItem
+                cardBackground={cardBackgroundColor}
+                textColor={textColor}
+                organization="Indian Army"
+                urlType="Certificate"
+                imageUrl={process.env.PUBLIC_URL + "/indianArmy.png"}
+                url="https://drive.google.com/file/d/1GG3aImLWPqQCCX1ZPVX9nIycRrzDPt8y/view?usp=sharing"
+                details={[
+                  ["Role", "Project Intern"],
+                  ["Duration", "July 2021 - Present"],
+                ]}
+                details2={[
+                  [
+                    "Work",
+                    "Created POC (Proof of concept) for some working malwares (like Anubis) and tested their functionality",
+                    "Analyzed android security, loop holes, Android Internals working.",
+                    "Reverse engineering, detection , analysis of malwares working and Android Smali Patching.",
+                  ],
+                  ["Tools Used", "Apktool, Frida, Jadx-Gui, JD-Gui, Retrofit"],
+                  ["Language Used", "Java, XML, Kotlin, C++, Smali, Sqlite"],
+                ]}
+              />
+            </div>
 
-          <div className="carousel-item">
-            <ExperienceItem
-              cardBackground={cardBackgroundColor}
-              textColor={textColor}
-              role="Internship"
-              name="Shree Ramsena"
-              urlType="Play Store"
-              duration="January 2022 - May 2023"
-              imageUrl={process.env.PUBLIC_URL + "/ramsena.png"}
-              url="https://play.google.com/store/apps/details?id=com.ramsena.android"
-              languageUsed="Java, XML"
-              toolsUsed="Firebase, FCM , Retrofit"
-              details={[
-                "Created complete Social Media app using PHP Apis and later with Firebase, with user interest's post.",
-                "Implemented live chat feature with encryption, super user role, Admin features.",
-                "Implemented one-one and one-many personalised notifcation (custom and automatic).",
-              ]}
-            />
-          </div>
-          <div className="carousel-item">
-            <ExperienceItem
-              cardBackground={cardBackgroundColor}
-              textColor={textColor}
-              role="Club Coordinator"
-              name="NJACK IIT Patna"
-              urlType="NJACK - Website"
-              imageUrl={process.env.PUBLIC_URL + "/njack.jpg"}
-              url="https://njack.iitp.ac.in/"
-              duration="October 2021 - May 2023"
-              details={[
-                "Coordinator of Not Just Another Coding Klub (NJACK) DEV and Open-Source department.",
-                "Organised India's one of the biggest Month long Hackathon - Codepeak (formly NWOC) with IIT Guwahati's Coding Club.",
-                "Organized various dev related events in college.",
-              ]}
-            />
+            <div>
+              <ExperienceItem
+                cardBackground={cardBackgroundColor}
+                textColor={textColor}
+                organization="Shree Ramsena"
+                urlType="Play Store"
+                imageUrl={process.env.PUBLIC_URL + "/ramsena.png"}
+                url="https://play.google.com/store/apps/details?id=com.ramsena.android"
+                details={[
+                  ["Role", "Internship"],
+                  ["Duration", "January 2022 - May 2023"],
+                ]}
+                details2={[
+                  [
+                    "Work",
+                    "Created complete Social Media app using PHP Apis and later with Firebase, with user interest's post.",
+                    "Implemented live chat feature with encryption, super user role, Admin features.",
+                    "Implemented one-one and one-many personalised notifcation (custom and automatic).",
+                  ],
+                  ["Tools Used", "Firebase, FCM , Retrofit"],
+                  ["Language Used", "Java, XML"],
+                ]}
+              />
+            </div>
+            <div>
+              <ExperienceItem
+                cardBackground={cardBackgroundColor}
+                textColor={textColor}
+                organization="NJACK IIT Patna"
+                urlType="NJACK - Website"
+                imageUrl={process.env.PUBLIC_URL + "/njack.jpg"}
+                url="https://njack.iitp.ac.in/"
+                details={[
+                  ["Role", "Club Coordinator"],
+                  ["Duration", "October 2021 - May 2023"],
+                ]}
+                details2={[
+                  [
+                    "Work",
+                    "Coordinator of Not Just Another Coding Klub (NJACK) DEV and Open-Source department.",
+                    "Organised India's one of the biggest Month long Hackathon - Codepeak (formly NWOC) with IIT Guwahati's Coding Club.",
+                    "Organized various dev related events in college.",
+                  ],
+                ]}
+              />
+            </div>
           </div>
         </div>
-        <a
-          className="carousel-control-prev"
-          href="#carouselExampleIndicators"
-          role="button"
-          data-slide="prev"
-        >
-          <span
-            className="carousel-control-prev-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="sr-only">Previous</span>
-        </a>
-        <a
-          className="carousel-control-next"
-          href="#carouselExampleIndicators"
-          role="button"
-          data-slide="next"
-        >
-          <span
-            className="carousel-control-next-icon"
-            aria-hidden="true"
-          ></span>
-          <span className="sr-only">Next</span>
-        </a>
       </div>
     </div>
   );
--- src/Components/ExperienceItem.js
+++ src/Components/ExperienceItem.js
@@ -3,95 +3,135 @@ import React from "react";
 export default function ExperienceItem(props) {
   return (
     <div
-      className="p-2 p-3 mb-5 row "
-      height="200px"
+      className="p-3 mt-4 shaadow"
       style={{ backgroundColor: props.cardBackground, color: props.textColor }}
     >
-      <div className="col-md-3">
+      <div>
+        <p className="my-2 mx-0 centered" style={{ fontSize: "1.35rem" }}>
+          <h3>{props.organization}</h3>
+        </p>
         <div
+          className="container my-3 row m-0 p-0"
           style={{
-            textAlign: "center",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
           }}
         >
-          <img
-            width={"80%"}
-            style={{ borderRadius: "50%" }}
-            className="my-1"
-            src={props.imageUrl}
-            alt="experienceImage"
-          />
-
-          <h5 className="m-1 header2-style mb-4">{props.name}</h5>
-        </div>
-      </div>
-
-      <div className="col-md-9">
-        <h5
-          className=" header3-style"
-          style={{
-            marginLeft: "5%",
-          }}
-        >
-          {props.role}
-        </h5>
-
-        <div className="container my-3">
-          <ul>
-            {props.details &&
-              props.details.map((data) => {
-                return (
-                  <li className="description-style" key={data}>
-                    {data}
-                  </li>
-                );
-              })}
-
-            <li className="description-style">
-              <b>Duration: </b>
-              {props.duration}
-            </li>
-            <li
-              className="description-style"
+          <div className="col-xl-2 col-lg-3 col-md-4 col-sm-5 ">
+            <div
               style={{
-                display: props.languageUsed == null ? "none" : "",
+                textAlign: "center",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100%",
+                flexDirection: "column",
+              }}
+              className="m-2"
+            >
+              <img
+                style={{ width: "100%", height: "auto" }}
+                className="my-0"
+                src={props.imageUrl}
+                alt="experienceImage"
+              />
+              <div className="py-3 d-sm-none"></div>
+            </div>
+          </div>
+          <div className="col-xl-10 col-lg-9 col-md-8 col-sm-7">
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                flexDirection: "column",
+                width: "100%",
+                height: "100%",
               }}
             >
-              <b>Languages Used: </b>
-              {props.languageUsed}
-            </li>
-            {props.toolsUsed ? (
-              <li className="description-style">
-                <b>Tools Used: </b>
-                {props.toolsUsed}
-              </li>
-            ) : (
-              <div style={{ margin: 40 }}></div>
-            )}
-          </ul>
+              {props.details &&
+                props.details.map((data, index) => {
+                  return (
+                    <div className="mb-2 p-0" key={index}>
+                      <div className="form-floating p-0 m-0">
+                        <div
+                          className="form-control"
+                          id={`dataDisplay_${index}`}
+                          style={{
+                            fontSize: "1rem",
+                            height: "100%",
+                            overflowY: "auto",
+                          }}
+                        >
+                          {data &&
+                            data.map((res, idx) => {
+                              return (
+                                idx !== 0 && <p className="m-0 p-0">{res}</p>
+                              );
+                            })}
+                        </div>
+
+                        <label
+                          htmlFor={`dataDisplay_${index}`}
+                          style={{ fontSize: "1rem" }}
+                        >
+                          {data[0]}
+                        </label>
+                      </div>
+                    </div>
+                  );
+                })}
+            </div>
+          </div>
         </div>
-        <div
-          style={{
-            textAlign: "center",
+        <div className="container my-3 row mx-0">
+          {props.details2 &&
+            props.details2.map((data, index) => {
+              return (
+                <div className="mb-2 p-0 " key={index}>
+                  <div className="form-floating p-0 m-0">
+                    <div
+                      className="form-control"
+                      id={`dataDisplay_${index}`} // You can use a dynamic ID if needed
+                      style={{
+                        fontSize: "1rem",
+                        // minHeight: "100px",
+                        height: "100%",
+                        overflowY: "auto",
+                      }}
+                    >
+                      {data &&
+                        data.map((res, idx) => {
+                          return idx !== 0 && <p className="m-0 p-0">{res}</p>;
+                        })}
+                    </div>
+
+                    <label
+                      htmlFor={`dataDisplay_${index}`}
+                      style={{ fontSize: "1rem" }}
+                    >
+                      {data[0]}
+                    </label>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+      <div
+        style={{
+          textAlign: "center",
+        }}
+      >
+        <button
+          type="button"
+          class="btn btn-outline-primary"
+          onClick={(e) => {
+            window.open(props.url, "_blank");
           }}
         >
-          <h4
-            style={{
-              textAlign: "start",
-              display: props.urlType == null ? "none" : "",
-              marginLeft: "5%",
-            }}
-          >
-            <a
-              href={props.url}
-              target="_blank"
-              rel="noreferrer"
-              className="bold-underline-link"
-              style={{ color: props.textColor }}
-            >
-              {props.urlType}
-            </a>
-          </h4>
-        </div>
+          {props.urlType}
+        </button>
       </div>
     </div>
   );
