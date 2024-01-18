5aebbd52d17f64d2adb2929247ab872fbf3c5a5d
Sandeep Mishra <sandeep03edu@gmail.com>
2024-01-19 02:50:52 +0530

    Updated Intro and cp skills width

--- src/Components/CompetitiveSkills.js
+++ src/Components/CompetitiveSkills.js
@@ -2,95 +2,98 @@ import React from "react";
 
 const CompetitiveSkills = (props) => {
   return (
-    <div
-      className="shadow p-3 rounded"
-      style={{
-        marginTop: "8%",
-        marginRight: "1%",
-        marginLeft: "1%",
-        backgroundColor: props.cardBackground,
-        color: props.textColor,
-      }}
-    >
-      <h2
-        className="center-container header2-style mb-4"
-        style={{ fontFamily: "Manrope" }}
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <div
+        className="shadow p-3 rounded"
+        style={{
+          marginTop: "8%",
+          marginRight: "1%",
+          marginLeft: "1%",
+          backgroundColor: props.cardBackground,
+          color: props.textColor,
+          width: "80%",
+        }}
       >
-        Competitive Programming
-      </h2>
-      <div className="table-responsive ">
-        <table className="table table-striped table-row-header table-bordered ">
-          <thead>
-            <tr>
-              <th scope="col">Platform</th>
-              <th scope="col">Username</th>
-              <th scope="col">Rating</th>
-              <th scope="col">Best Rank</th>
-            </tr>
-          </thead>
-          <tbody className="table-row description-style">
-            <tr>
-              <td>Codeforces</td>
-              <td>
-                <a
-                  className="bold-underline-link"
-                  href="https://codeforces.com/profile/sandeep03edu"
-                >
-                  Sandeep03edu
-                </a>
-              </td>
-              <td>1552 (Specialist)</td>
-              <td>
-                <a
-                  className="bold-underline-link"
-                  href="https://codeforces.com/contest/1729/standings/participant/140126541#p140126541"
-                >
-                  432 - Codeforces Round 820 (Div. 3)
-                </a>
-              </td>
-            </tr>
-            <tr>
-              <td>Leetcode</td>
-              <td>
-                <a
-                  className="bold-underline-link"
-                  href="https://leetcode.com/sandeep03edu/"
-                >
-                  Sandeep03edu
-                </a>
-              </td>
-              <td>1910 (Knight)</td>
-              <td>
-                <a
-                  className="bold-underline-link"
-                  href="https://leetcode.com/contest/biweekly-contest-109/ranking"
-                >
-                  561 - Biweekly Contest 109
-                </a>
-              </td>
-            </tr>
-            <tr>
-              <td>Atcoder</td>
-              <td>
-                <a
-                  className="bold-underline-link"
-                  href="https://atcoder.jp/users/sandeep03edu"
-                >
-                  Sandeep03edu
-                </a>
-              </td>
-              <td>1017 (5 Kyu)</td>
-              <td>
-                <a
-                  className="bold-underline-link"
-                  href="https://atcoder.jp/contests/abc311/standings?watching=sandeep03edu"
-                >
-                  736 - Toyota AtCoder Beginner Contest 311
-                </a>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <h2
+          className="center-container header2-style mb-4"
+          style={{ fontFamily: "Manrope" }}
+        >
+          Competitive Programming
+        </h2>
+        <div className="table-responsive ">
+          <table className="table table-striped table-row-header table-bordered ">
+            <thead>
+              <tr>
+                <th scope="col">Platform</th>
+                <th scope="col">Username</th>
+                <th scope="col">Rating</th>
+                <th scope="col">Best Rank</th>
+              </tr>
+            </thead>
+            <tbody className="table-row description-style">
+              <tr>
+                <td>Codeforces</td>
+                <td>
+                  <a
+                    className="bold-underline-link"
+                    href="https://codeforces.com/profile/sandeep03edu"
+                  >
+                    Sandeep03edu
+                  </a>
+                </td>
+                <td>1552 (Specialist)</td>
+                <td>
+                  <a
+                    className="bold-underline-link"
+                    href="https://codeforces.com/contest/1729/standings/participant/140126541#p140126541"
+                  >
+                    432 - Codeforces Round 820 (Div. 3)
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>Leetcode</td>
+                <td>
+                  <a
+                    className="bold-underline-link"
+                    href="https://leetcode.com/sandeep03edu/"
+                  >
+                    Sandeep03edu
+                  </a>
+                </td>
+                <td>1910 (Knight)</td>
+                <td>
+                  <a
+                    className="bold-underline-link"
+                    href="https://leetcode.com/contest/biweekly-contest-109/ranking"
+                  >
+                    561 - Biweekly Contest 109
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>Atcoder</td>
+                <td>
+                  <a
+                    className="bold-underline-link"
+                    href="https://atcoder.jp/users/sandeep03edu"
+                  >
+                    Sandeep03edu
+                  </a>
+                </td>
+                <td>1017 (5 Kyu)</td>
+                <td>
+                  <a
+                    className="bold-underline-link"
+                    href="https://atcoder.jp/contests/abc311/standings?watching=sandeep03edu"
+                  >
+                    736 - Toyota AtCoder Beginner Contest 311
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -3,87 +3,89 @@ import Typical from "react-typical";
 
 export default function Intro(props) {
   return (
-    <div
-      className="row"
-      style={{
-        color: props.textColor,
-        padding: "5% 0%",
-        width: "100%",
-      }}
-    >
-      <div className="col-md-6 image-top-margin">
-        <img
-          style={{
-            borderRadius: "5%",
-            width: "100%",
-            height: "100%",
-          }}
-          src={process.env.PUBLIC_URL + "/profile_intro.jpeg"}
-          alt="Profile"
-        />
-      </div>
-
+    <div style={{ display: "flex", justifyContent: "center" }}>
       <div
-        className="col-md-5 header-style"
-        style={{ margin: "auto", textAlign: "center", fontFamily: "Manrope" }}
+        className="row"
+        style={{
+          color: props.textColor,
+          padding: "10% 0%",
+          width: "90%",
+        }}
       >
-        <h3
-          style={{
-            margin: 20,
-          }}
-        >
-          I am{" "}
-          <span>
-            <Typical
-              steps={[
-                "Android Developer",
-                2000,
-                "Competitive Programmer",
-                2000,
-                "Electrical and Electronics Engineer",
-                2000,
-                "Web Developer",
-                2000,
-                "Cybersecurity Enthusiast",
-                2000,
-              ]}
-              loop={Infinity}
-              wrapper="p"
-            />
-          </span>
-        </h3>
+        <div className="col-md-6 image-top-margin">
+          <img
+            style={{
+              borderRadius: "5%",
+              width: "100%",
+              height: "100%",
+            }}
+            src={process.env.PUBLIC_URL + "/profile_intro.jpeg"}
+            alt="Profile"
+          />
+        </div>
 
         <div
-          className="shadow"
-          style={{
-            backgroundColor: "#0C0084",
-            width: "max-content",
-            borderRadius: 50,
-            margin: "auto",
-          }}
-          onClick={(e) => {
-            console.log("CLicked!!");
-            e.preventDefault();
-            window.scrollTo({
-              top: document.querySelector("#aboutSection").offsetTop - 80,
-              behavior: "smooth",
-            });
-          }}
+          className="col-md-5 header-style"
+          style={{ margin: "auto", textAlign: "center", fontFamily: "Manrope" }}
         >
-          <h5
+          <h3
             style={{
-              textAlign: "center",
-              paddingLeft: 40,
-              paddingRight: 40,
-              paddingTop: 10,
-              paddingBottom: 10,
-              color: "#fff",
-              fontFamily: "Roboto",
-              fontWeight: "500",
+              margin: 20,
             }}
           >
-            About Me
-          </h5>
+            I am{" "}
+            <span>
+              <Typical
+                steps={[
+                  "Android Developer",
+                  2000,
+                  "Competitive Programmer",
+                  2000,
+                  "Electrical and Electronics Engineer",
+                  2000,
+                  "Web Developer",
+                  2000,
+                  "Cybersecurity Enthusiast",
+                  2000,
+                ]}
+                loop={Infinity}
+                wrapper="p"
+              />
+            </span>
+          </h3>
+
+          <div
+            className="shadow"
+            style={{
+              backgroundColor: "#0C0084",
+              width: "max-content",
+              borderRadius: 50,
+              margin: "auto",
+            }}
+            onClick={(e) => {
+              console.log("CLicked!!");
+              e.preventDefault();
+              window.scrollTo({
+                top: document.querySelector("#aboutSection").offsetTop - 100,
+                behavior: "smooth",
+              });
+            }}
+          >
+            <h5
+              style={{
+                textAlign: "center",
+                paddingLeft: 40,
+                paddingRight: 40,
+                paddingTop: 10,
+                paddingBottom: 10,
+                color: "#fff",
+                fontFamily: "Roboto",
+                fontWeight: "500",
+              }}
+            >
+              About Me
+            </h5>
+          </div>
         </div>
       </div>
     </div>
